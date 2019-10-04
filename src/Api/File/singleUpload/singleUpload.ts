97bd904a3cd240f5ccb2fad72dcb997d564bedde
changju <tjerry3@naver.com>
2019-10-04 21:25:35 +0900

    upload to s3

--- src/Api/File/singleUpload/singleUpload.ts
+++ src/Api/File/singleUpload/singleUpload.ts
@@ -1,19 +1,25 @@
-import uuid from "uuid/v1";
-import aws from "aws-sdk";
+import * as aws from "aws-sdk";
 
 export default {
   Mutation: {
     singleUpload: async (obj, { file }): Promise<string> => {
       const { filename, mimetype, createReadStream, encoding } = await file;
+      console.log(mimetype, encoding);
       const s3 = new aws.S3({
         accessKeyId: process.env.ACCESSKEYID,
         secretAccessKey: process.env.SECRET_ACCESSKEYID
       });
-      const key = uuid() + "-" + file.name;
+      const key =
+        new Date()
+          .toISOString()
+          .replace(/T/, " ") // replace T with a space
+          .replace(/\..+/, "") +
+        "-" +
+        filename;
       const stream = createReadStream();
       const response = await s3
         .upload({
-          Bucket: "jerrynim-blog",
+          Bucket: process.env.BUCKET_NAME,
           Key: key,
           ACL: "public-read",
           Body: stream
@@ -22,7 +28,7 @@ export default {
         .catch();
       const url = response.Location;
 
-      return "returnFile";
+      return url;
     }
   }
 };
