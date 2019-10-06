996d0f211f7ca907900d56e55d554cdeff1d2b22
changju <tjerry3@naver.com>
2019-10-06 19:56:27 +0900

    gulp change

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,10 @@ var gulp = require("gulp");
 var ts = require("gulp-typescript");
 gulp.task("babel", function() {
   //gulp babel 입력시 실행
+  var tsconfig = require("./tsconfig.json");
+
   return gulp
     .src("src/**/*.ts")
-    .pipe(
-      ts({
-        noImplicitAny: false // Warn on expressions and declarations with an implied 'any' type.
-      })
-    )
+    .pipe(ts(tsconfig.compilerOptions))
     .pipe(gulp.dest("build")); //build에 저장
 });
